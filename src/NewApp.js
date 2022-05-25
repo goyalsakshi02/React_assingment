d0757b206e712d8737d1a0cc3dc318b3bf3bd96c
Sakshi Goyal <sakshi.goyal@dianapps.com>
2022-05-25 18:12:19 +0530

    update

--- src/NewApp.js
+++ src/NewApp.js
@@ -4,24 +4,45 @@ import axios from "axios"
 const NewApp = () => {
     
     const [apiArr, setApiArr] = useState([]);
+    const [categoryData, setCategoryData] = useState(['general']);
     
     useEffect(() => {
         axios.get("https://newsapi.org/v2/top-headlines/sources?apiKey=baebdb27500542c78c84eb4c5d17d745").then(apiArr=> {
             console.log("data ==>>",apiArr)
-            setApiArr(apiArr?.data.sources)
+            // setApiArr(apiArr?.data?.sources)
+            // console.log(apiArr.data.sources);
+            setApiArr(apiArr.data.sources);
         }).catch(error => {
             console.log(error)
         });
       }, []);
 
+
+     const handlecatogary = (value) => {
+         let arr =[]
+         arr = apiArr?.map((item, id) =>
+            item?.category
+        )
+
+     }
+
+      apiArr?.map((item, id) =>
+        console.log(item)
+        
+        )
+
+
       return(
         <>
-           <div class>Asssignment</div>
-            <div class='data'>
+           <div>Asssignment</div>
+            <div className='data' >
                 {
-                apiArr?.map(item =>
                     
-                )
+                apiArr?.map((item, id) =>
+                  <h1 key={id}>{item?.category}</h1>  
+                  
+                 )
+                 
             }
             </div>
 
