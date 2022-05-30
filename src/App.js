909b029a861e176506f10c3993900d6d69c4ed51
Sakshi Goyal <sakshi.goyal@dianapps.com>
2022-05-30 18:19:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import NewApp from './NewApp';
-import Sakshi from './Sakshi'
+
 // import FetchFunc from './FetchFunc';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <FetchFunc /> */}
-        {/* <NewApp /> */}
-        <Sakshi />
+         <NewApp />
+        
       </header>
     </div>
   );
--- src/NewApp.js
+++ src/NewApp.js
@@ -1,112 +1,82 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import moment from "moment";
+import moment from 'moment';
 
 const NewApp = () => {
   const [apiArr, setApiArr] = useState([]);
-  const [allTitle, setAllTitle] = useState("");
-  const [descrip, setDescrip] = useState("");
-  const [title, setTitle] = useState("");
-  const [fakData, setFakData] = useState("");
+  const [allTitle, setAllTitle] = useState('');
+  const [descrip, setDescrip] = useState(""); 
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
-    // axios
-    //   .get(
-    //     "https://newsapi.org/v2/top-headlines?country=in&apiKey=589773f1ae1943cb9ce6fef69f973fb8"
-    //   )
-    //   .then((apiArr) => {
-    //     setApiArr(apiArr?.data?.articles);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-    fakeApi();
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=589773f1ae1943cb9ce6fef69f973fb8"
+      )
+      .then((apiArr) => {
+        setApiArr(apiArr?.data?.articles);
+      })
+      .catch((error) => { console.log(error)
+       
+      });
   }, []);
+  
+  
+    const unique = [...new Set(apiArr.map((item) => item?.source?.name))];
 
-  const unique = [...new Set(apiArr.map((item) => item?.source?.name))];
+    const handleSource = (item , i) =>{
+    const alTitlDate = [ ...new Set(apiArr.map((item) => item))]
+    
+   
+    setAllTitle(alTitlDate)
+    setTitle([])
+   
+    }
 
-  const handleSource = (item, i) => {
-    const alTitlDate = [...new Set(apiArr.map((item) => item))];
-    setAllTitle(alTitlDate);
-    setTitle([]);
-    setDescrip([]);
-  };
+   
 
   function handleCatData(catName) {
     var newArr = apiArr.filter((item) => item?.source?.name == catName);
-    setTitle(newArr);
-    setAllTitle([]);
+    setTitle(newArr)
+    setAllTitle([])
+    
   }
 
-  function handleDescription(value) {
-    setDescrip(value);
+  function handleDescription(value) { 
+    setDescrip(value)
   }
-  const fakeApi = () => {
-    axios
-      .get("https://fakestoreapi.com/products")
-      .then((res) => {
-        console.log("fake :", res?.data);
-        // setFakData(res?.dat?.category)
-      })
-      .catch((error) => {
-        console.log(error);
-       
-
-      });
-
-    };
-
-    console.log("delfefre+>f4fg", fakData )
 
   return (
     <div className="container">
+      
       <div className="leftpane">
-        <div onClick={(item) => handleSource(item)}>sources</div>
-
-        {unique.map((item, i) => (
-          <div onClick={() => handleCatData(item)} key={i} className="data">
-            {item}
-          </div>
+          
+          <div className="data" onClick={(item) => handleSource(item)} >sources</div>
+          
+        
+        {
+            unique.map((item, i) => (
+           
+           <div className="data" onClick={() => handleCatData(item)} key={i}>
+                {item}
+            </div>
         ))}
-      </div>
 
-      <div className="middlepane">
-        {allTitle &&
-          allTitle.map((value) => (
-            <div className="titles" onClick={() => handleDescription(value)}>
-              <div className="titles">{value.title}</div>
-              <div className="date">
-                {moment(value.publishedAt).format("DD/MM/YYYY")}
-              </div>
-            </div>
-          ))}
-
-        {title &&
-          title.map((value) => (
-            <div onClick={() => handleDescription(value)}>
-              <div className="titles">{value.title}</div>
-              <div className="date">
-                {moment(value.publishedAt).format("DD/MM/YYYY")}
-              </div>
-              <div></div>
-            </div>
-          ))}
-      </div>
-
-      <div className="rightpane">
-        {descrip && (
-          <div>
-            <div className="image">
-              <img
-                style={{ width: "80px", height: "80px" }}
-                src={descrip.urlToImage}
-              ></img>
-            </div>
-            <div className="descri[">{descrip.description}</div>
-          </div>
-        )}
-      </div>
+        </div>
+     
+     
+     <div className="middlepane">
+        
+        {allTitle && allTitle.map((value) => <div onClick= {() => handleDescription(value)}><div className="title">{value.title}</div><div className="date">{moment(value.publishedAt).format("DD/MM/YYYY")}</div></div>)} 
+     
+        {title && title.map((value) => <div onClick= {() => handleDescription(value)}><div className="title">{value.title}</div><div className="date">{moment(value.publishedAt).format("DD/MM/YYYY")}</div><div></div></div>)}</div>
+     
+        <div className="rightpane">
+           
+            {descrip && <div><div className="image"><img style={{width:"100%",height:"auto"}}src={descrip.urlToImage}></img></div><div className="descrip">{descrip.description}</div></div>}</div>
+    
+   
     </div>
   );
 };
